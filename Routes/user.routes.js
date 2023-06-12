0eb742d25ea461e150e2bed729f17eac6472caa2
Akash5Chaurasiya <anitachaurasiya9936@gmail.com>
2023-06-12 19:01:12 +0530

    done

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const bcrypt=require("bcrypt");
+const bcrypt=require('bcrypt');
 const jwt=require('jsonwebtoken');
 const { userModel } = require('../model/users.model');
 const userRouter=express.Router();
--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const { userRouter } = require('./Routes/user.routes');
 const { connection } = require("./db");
 const { postRouter } = require("./Routes/posts.routes");
 const cors=require("cors")
-app.use(cors())
 require('dotenv').config()
 const app=express();
+app.use(cors())
 app.use(express.json())
 app.use("/users",userRouter)
 app.use("/posts",postRouter)
