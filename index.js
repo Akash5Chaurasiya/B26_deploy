a5376ce91805a9b93b9b9ebac93215d82ba50add
Akash5Chaurasiya <anitachaurasiya9936@gmail.com>
2023-06-12 18:18:49 +0530

    done

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { userRouter } = require('./Routes/user.routes');
 const { connection } = require("./db");
 const { postRouter } = require("./Routes/posts.routes");
 const cors=require("cors")
-require('dotenv').config()
 app.use(cors())
+require('dotenv').config()
 const app=express();
 app.use(express.json())
 app.use("/users",userRouter)
